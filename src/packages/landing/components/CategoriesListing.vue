17ad79f096a1d441a78192f9542b586a08c38b70
paullaster <paullasterokoth98@gmail.com>
2024-08-17 04:19:01 +0300

    update categories to point api data

--- src/packages/landing/components/CategoriesListing.vue
+++ src/packages/landing/components/CategoriesListing.vue
@@ -12,50 +12,13 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { useSetupStore } from '@/store';
+import { storeToRefs } from 'pinia';
 
-const categories = ref([
-  {
-    cid: 'Fried',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried2',
-    description: 'This is Fried Two Pane Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried3',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried4',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried6',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried5',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'Fried7',
-    description: 'Fried Foods',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  },
-  {
-    cid: 'FriedLastly',
-    description: 'Fried Lastly Food Today',
-    image: 'http://192.168.0.103:3500/public/image/products/AAA084LGOVUAM.png'
-  }
-])
+
+const setupStore = useSetupStore();
+const { categories } = storeToRefs(setupStore);
+
 </script>
 
 <style lang="scss" scoped>
