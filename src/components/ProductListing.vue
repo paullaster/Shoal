461107f3294781f214b4ad187c0c0ed818963a34
paullaster <paullasterokoth98@gmail.com>
2024-10-01 01:03:57 +0300

    update product listing display

--- src/components/ProductListing.vue
+++ src/components/ProductListing.vue
@@ -1,17 +1,21 @@
 <template>
   <article class="product-listing-wrapper cool-shadow cool-borderradius" 
   @click="()=>router.push({name: 'productDetails', params: {category: product.category,productId: stringToBase64AndReverse.toBase64String(product.pid)}})"
+  >
+  <div class="productlisting-image"
   :style="
   `
   background-image: linear-gradient(to bottom, #4d80807c, #4d808080), url(${product?.Images[0].url});
   `
   "
   >
+
+  </div>
     <div class="product-details">
-      <p>
+      <p class="product-details-name">
         {{ product?.name }}
       </p>
-      <p>{{ currency }} {{ product?.price }}</p>
+      <p class="product-details-price">{{ currency }} {{ product?.price }}</p>
     </div>
     <div class="available"></div>
   </article>
