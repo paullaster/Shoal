ce8d087db65cd42d21addcc5bda2ab920aeb016a
paullaster <paullasterokoth98@gmail.com>
2025-04-10 05:24:58 +0300

    feat(feat(feat(chore(core - Added  in app/components/PaginationComponent.vue): chore index.vue): add imprest-filter): add imprest-filter): add -   <main class="complete-profile"> functionality - Added -   <main class="complete-profile"> in src/assets/style/profile.css

--- src/packages/auth/components/ProfileComponent.vue
+++ src/packages/auth/components/ProfileComponent.vue
@@ -1,76 +1,63 @@
 <template>
-   <main class="complete-profile">
-    <section class="complete-profile-card">
-        <v-card elevation="0">
-            <v-card-title class="headline">{{userData[3] === 'newAccount' ? 'Complete Profile' : 'Enter your password'}}</v-card-title>
-            <v-card-text>
-               <v-form ref="profileForm">
-                <v-row>
-                    <v-col cols="12" sm="6">
-                        <v-label>Email</v-label>
-                        <v-text-field variant="outlined" v-model="user.email" :readonly="userData[2] === 'email'" :rules="rules.email"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
-                        <v-label>First Name</v-label>
-                        <v-text-field variant="outlined" v-model="user.firstName" placeholder="for example: John" :rules="rules.firstName"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
-                        <v-label>Last Name</v-label>
-                        <v-text-field variant="outlined" v-model="user.lastName" placeholder="for example: Doe" :rules="rules.lastName"></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
-                        <v-label>Phone number</v-label>
-                        <v-text-field 
-                        variant="outlined" 
-                        v-model="user.phoneNumber" 
-                        placeholder="for example: +254 700258098" 
-                        :readonly="userData[2] === 'phone'"
-                        :rules="rules.phoneNumber"
-                        @update:modelValue="formatPhoneNumberInput"
-                        ></v-text-field>
-                    </v-col>
-                    <v-col cols="12" sm="6">
-                        <v-label>Password</v-label>
-                        <v-text-field 
-                        variant="outlined" 
-                        v-model="user.password" 
-                        :type="showPassword? 'text' : 'password'" 
-                        placeholder="minimum 8 characters" 
-                        :rules="rules.password"
-                        
-                        >
-                        <template v-slot:append-inner>
-                            <v-icon 
-                            @click="toggleShowPassword" 
-                            :color="ColorHelper.colorsHelper('secondary')"
-                            style="
+    <main class="complete-profile">
+        <section class="complete-profile-card">
+            <v-card elevation="0">
+                <v-card-title style="width: 100%;">
+                    {{ userData[3] === 'newAccount' ? 'Complete Profile' : 'Enter your password' }}
+                </v-card-title>
+                <v-card-text style="padding: 0 !important;">
+                    <v-form ref="profileForm">
+                        <v-row style="margin: 0 !important;">
+                            <v-col cols="12" sm="6">
+                                <v-label>Email</v-label>
+                                <v-text-field variant="outlined" v-model="user.email"
+                                    :readonly="userData[2] === 'email'" :rules="rules.email"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
+                                <v-label>First Name</v-label>
+                                <v-text-field variant="outlined" v-model="user.firstName"
+                                    placeholder="for example: John" :rules="rules.firstName"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
+                                <v-label>Last Name</v-label>
+                                <v-text-field variant="outlined" v-model="user.lastName" placeholder="for example: Doe"
+                                    :rules="rules.lastName"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" v-if="userData[3] === 'newAccount'">
+                                <v-label>Phone number</v-label>
+                                <v-text-field variant="outlined" v-model="user.phoneNumber"
+                                    placeholder="for example: +254 700258098" :readonly="userData[2] === 'phone'"
+                                    :rules="rules.phoneNumber"
+                                    @update:modelValue="formatPhoneNumberInput"></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                                <v-label>Password</v-label>
+                                <v-text-field variant="outlined" v-model="user.password"
+                                    :type="showPassword ? 'text' : 'password'" placeholder="minimum 8 characters"
+                                    :rules="rules.password">
+                                    <template v-slot:append-inner>
+                                        <v-icon @click="toggleShowPassword"
+                                            :color="ColorHelper.colorsHelper('secondary')" style="
                             background-color: #ED1E79 !important;
                             padding: 1rem !important;
                             border-radius: .4rem !important;
-                            "
-                            >{{ showPassword? 'mdi-lock-outline' : 'mdi-lock-open-variant-outline' }}</v-icon>
-                        </template>
-                    </v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                        <v-btn
-                        :color="ColorHelper.colorsHelper('primary')"
-                        @click="updateProfile"
-                        :loading="loading"
-                        :disabled="loading"
-                        :block="mdAndDown"
-                        size="x-large"
-                        >
-                           {{userData[3] === 'newAccount' ? 'Update Profile' : 'Continue'}}
-                        </v-btn>
-                    </v-col>
-                </v-row>
-               </v-form>
-            </v-card-text>
-        </v-card>
+                            ">{{ showPassword ? 'mdi-lock-outline' : 'mdi-lock-open-variant-outline' }}</v-icon>
+                                    </template>
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                                <v-btn :color="ColorHelper.colorsHelper('primary')" @click="updateProfile"
+                                    :loading="loading" :disabled="loading" :block="mdAndDown" size="x-large">
+                                    {{ userData[3] === 'newAccount' ? 'Update Profile' : 'Continue' }}
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-form>
+                </v-card-text>
+            </v-card>
 
-    </section>
-   </main>
+        </section>
+    </main>
 </template>
 
 <script setup>
@@ -97,7 +84,7 @@ const profileForm = ref(null);
 
 // APP STORE
 const authStore = useAuth();
-const cartStore  = useCartStore();
+const cartStore = useCartStore();
 const { loading } = storeToRefs(authStore);
 
 // HOOK
@@ -122,23 +109,23 @@ onMounted(() => {
 const rules = computed(() => {
     return {
         firstName: [
-            v =>!!v || 'First name is required',
+            v => !!v || 'First name is required',
             v => /^[a-zA-Z\s]+$/.test(v) || 'Name should only contain letters and spaces'
         ],
         lastName: [
-            v =>!!v || 'Last name is required',
+            v => !!v || 'Last name is required',
             v => /^[a-zA-Z\s]+$/.test(v) || 'Name should only contain letters and spaces'
         ],
         email: [
-            v =>!!v || 'Email is required',
+            v => !!v || 'Email is required',
             v => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v) || 'Invalid email format'
         ],
         password: [
-            v =>!!v || 'Password is required',
+            v => !!v || 'Password is required',
             v => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v) || 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character'
         ],
         phoneNumber: [
-            v =>!!v || 'Phone number is required',
+            v => !!v || 'Phone number is required',
             v => /^\+\d{1,4}\s\d{1,15}$/.test(v) || 'Invalid phone number format'
         ]
     }
@@ -146,7 +133,7 @@ const rules = computed(() => {
 
 
 // COMPONENT METHODS
-async function updateProfile () {
+async function updateProfile() {
     try {
         const { valid } = await profileForm.value.validate();
         if (!valid) {
@@ -154,19 +141,19 @@ async function updateProfile () {
         }
 
         user.value.password = btoa(user.value.password);
-        if(userData.value[3] === 'newAccount') {
+        if (userData.value[3] === 'newAccount') {
             await authStore.updateProfile(user.value, userData.value);
-            user.value =  { ... user.value, name: '', [userData.value[2] === 'email' ? 'phoneNumber' : 'email']: '', password: ''};
-        }else {
-            await authStore.login({username: user.value.email, password: user.value.password});
-            user.value =  { ... user.value, password: ''};
+            user.value = { ...user.value, name: '', [userData.value[2] === 'email' ? 'phoneNumber' : 'email']: '', password: '' };
+        } else {
+            await authStore.login({ username: user.value.email, password: user.value.password });
+            user.value = { ...user.value, password: '' };
         }
     } catch (error) {
         useToast().error(error?.message);
     }
 }
-function toggleShowPassword () {
-    showPassword.value =!showPassword.value;
+function toggleShowPassword() {
+    showPassword.value = !showPassword.value;
 }
 
 function formatPhoneNumberInput(val) {
