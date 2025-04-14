58d4a39ebf8b1f5c166f82e1c597f2aa699a7762
paullaster <paullasterokoth98@gmail.com>
2025-04-14 04:53:29 +0300

    feat(feat(feat(chore(core - Added  in app/components/PaginationComponent.vue): chore index.vue): add imprest-filter): add imprest-filter): add      <div class="user-history-wrapper"> functionality - Added      <div class="user-history-wrapper"> in dev-dist/sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hu0d4tv13jo"
+    "revision": "0.ogdaul5036o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/packages/landing/components/UserHistory.vue
+++ src/packages/landing/components/UserHistory.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="user-history-wrapper">
-        <component-heading-with-button :history="userHistory"/>
+        <component-heading-with-button :history="userHistory" />
         <div class="row">
-            <ProductListing v-for="product in products" :key="product.pid" :product="product"/>
+            <ProductListing v-for="(product, index) in products" :key="index" :product="product" />
         </div>
     </div>
 </template>
@@ -16,6 +16,6 @@ import { storeToRefs } from 'pinia';
 
 // STORE
 const productStore = useProductStore();
-const {products} = storeToRefs(productStore);
+const { products } = storeToRefs(productStore);
 const userHistory = ref("Our products");
 </script>
